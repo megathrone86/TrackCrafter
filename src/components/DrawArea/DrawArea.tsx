a3c8d15cef5dc969a4fcd9d63e798627628da12b
khozeev.vladimir <khozeev.vladimir@nevlabs.ru>
2023-10-30 13:45:25 +0300

    реализовано перемещение камеры мышкой

--- src/components/DrawArea/DrawArea.tsx
+++ src/components/DrawArea/DrawArea.tsx
@@ -3,14 +3,16 @@ import "./DrawArea.scss";
 import { IRootState } from "../../store/store";
 import { useEffect, useRef, useState } from "react";
 
+type Point = { x: number; y: number };
+
 const pixelsToMeterRatio = 100;
 
 export function DrawArea() {
   const viewportRef = useRef(null);
-  const [viewportSize, setSize] = useState({ x: 0, y: 0 });
+  const [viewportSize, setSize] = useState<Point>({ x: 0, y: 0 });
 
-  const [camX, setCamX] = useState(0);
-  const [camY, setCamY] = useState(0);
+  const [camPos, setCamPos] = useState<Point>({ x: 0, y: 0 });
+  const [mouseStartPos, setMouseStartPos] = useState<Point | null>(null);
 
   const gridSize = useSelector((state: IRootState) => state.gridSize)
     .value as number;
@@ -24,17 +26,21 @@ export function DrawArea() {
 
   const gridCellSize = pixelsToMeterRatio * gridSize;
   const gridColumnsStart =
-    Math.trunc(camX / gridCellSize) * gridCellSize - gridCellSize - camX;
+    Math.trunc(camPos.x / gridCellSize) * gridCellSize -
+    gridCellSize -
+    camPos.x;
   const gridColumnsEnd =
-    Math.trunc((viewportSize.x + camX) / gridCellSize) * gridCellSize +
+    Math.trunc((viewportSize.x + camPos.x) / gridCellSize) * gridCellSize +
     gridCellSize -
-    camX;
+    camPos.x;
   const gridRowsStart =
-    Math.trunc(camY / gridCellSize) * gridCellSize - gridCellSize - camY;
-  const gridRowsEnd =
-    Math.trunc((viewportSize.y + camY) / gridCellSize) * gridCellSize +
+    Math.trunc(camPos.y / gridCellSize) * gridCellSize -
     gridCellSize -
-    camY;
+    camPos.y;
+  const gridRowsEnd =
+    Math.trunc((viewportSize.y + camPos.y) / gridCellSize) * gridCellSize +
+    gridCellSize -
+    camPos.y;
 
   const columns = [];
   for (let x = gridColumnsStart; x <= gridColumnsEnd; x += gridCellSize) {
@@ -46,7 +52,13 @@ export function DrawArea() {
   }
 
   return (
-    <div className="tc-DrawArea" ref={viewportRef}>
+    <div
+      className="tc-DrawArea"
+      ref={viewportRef}
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
+    >
       <button onClick={test} style={{ zIndex: 999999 }}>
         test
       </button>
@@ -54,30 +66,61 @@ export function DrawArea() {
       <div className="tc-DrawArea-grid">
         {columns.map((x) => (
           <div
+            key={`v${x}`}
             className="tc-DrawArea-grid-vertical-line"
             style={{ left: `${x}px` }}
           ></div>
         ))}
         {rows.map((y) => (
           <div
+            key={`h${y}`}
             className="tc-DrawArea-grid-horizontal-line"
             style={{ top: `${y}px` }}
           ></div>
         ))}
       </div>
 
-      <p>gridSize = {gridSize}</p>
-      <p>
-        viewportSize = {viewportSize.x}, {viewportSize.y}
-      </p>
-      <p>
-        cam: {camX}, {camY}
-      </p>
+      <div className="tc-DrawArea-debugInfo">
+        <p>gridSize = {gridSize}</p>
+        <p>
+          viewportSize = {viewportSize.x}, {viewportSize.y}
+        </p>
+        <p>
+          cam: {camPos.x}, {camPos.y}
+        </p>
+      </div>
     </div>
   );
 
-  function test() {
-    // setCamX(camX + 5);
-    setCamY(camY + 5);
+  function handlePointerDown(e: React.PointerEvent) {
+    if (viewportRef.current) {
+      setMouseStartPos({ x: e.screenX, y: e.screenY });
+      const element = viewportRef.current as HTMLDivElement;
+      element.setPointerCapture(e.pointerId);
+    }
   }
+
+  function handlePointerMove(e: React.PointerEvent, release?: boolean) {
+    if (mouseStartPos) {
+      const delta = {
+        x: e.screenX - mouseStartPos.x,
+        y: e.screenY - mouseStartPos.y,
+      };
+      setMouseStartPos({ x: e.screenX, y: e.screenY });
+
+      setCamPos({ x: camPos.x - delta.x, y: camPos.y - delta.y });
+
+      if (release && viewportRef.current) {
+        const element = viewportRef.current as HTMLDivElement;
+        element.releasePointerCapture(e.pointerId);
+        setMouseStartPos(null);
+      }
+    }
+  }
+
+  function handlePointerUp(e: React.PointerEvent) {
+    handlePointerMove(e, true);
+  }
+
+  function test() {}
 }
